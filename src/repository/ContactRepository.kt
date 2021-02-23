dbf4ba3b793594932e5ea1e25a8d5ded9815bc1b
Marc8s <marcus.ecomp@gmail.com>
2021-02-23 20:39:54 +0000

    Removendo contatos Business e Repository

--- src/repository/ContactRepository.kt
+++ src/repository/ContactRepository.kt
@@ -11,7 +11,16 @@ class ContactRepository {
         }
 
         fun delete(contact: ContactEntity){
-
+            //maneira mais pratica:
+            //contactList.remove(contact)
+            var index = 0
+            for(item in contactList.withIndex()){
+                if(item.value.name == contact.name && item.value.phone == contact.phone){
+                    index = item.index
+                    break
+                }
+            }
+            contactList.removeAt(index)
         }
 
         fun getList(): List<ContactEntity>{
