e6bb08001a76be5603655ffb1952823a4437be0b
Marc8s <marcus.ecomp@gmail.com>
2021-02-23 18:58:10 +0000

    Salvando contato repository

--- src/business/ContactBusiness.kt
+++ src/business/ContactBusiness.kt
@@ -1,5 +1,8 @@
 package business
 
+import entity.ContactEntity
+import repository.ContactRepository
+
 class ContactBusiness{
     fun validate(name: String, phone: String){
         if(name == ""){
@@ -20,10 +23,13 @@ class ContactBusiness{
 
     fun save(name: String, phone: String ){
         validate(name, phone)
-
+        val contact = ContactEntity(name, phone)
+        ContactRepository.save(contact)
     }
 
     fun delete(name: String, phone: String){
         validateDelete(name, phone)
+        val contact = ContactEntity(name, phone)
+        ContactRepository.delete(contact)
     }
 }
